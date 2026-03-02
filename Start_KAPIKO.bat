747aeeebce3917f222278997e4398e3ccc66ab8d
RayvherSky <Rayvhersky123@gmail.com>
2026-03-02 22:09:49 +0800

    Added background remover (Doesn't work on slow phones yet

---
+++ Start_KAPIKO.bat
@@ -0,0 +1,7 @@
+@echo off
+title KAPIKO QR Server
+echo ===================================================
+echo   KAPIKO RESUME SYSTEM IS ONLINE
+echo   Please minimize this window, do not close it.
+echo ===================================================
+ngrok http --domain=garnet-noncontumacious-scrawly.ngrok-free.dev 80
